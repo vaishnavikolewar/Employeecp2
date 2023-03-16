03e5685a263d06791e0201c2f2cb1949dfc4f317
vaishnavikolewar <vkolewar@gmail.com>
2023-03-16 11:33:42 +0000

    solution

---
+++ src/main/java/com/example/employee/EmployeeRepository.java
@@ -0,0 +1,17 @@
+package com.example.employee;
+
+import java.util.*;
+import com.example.employee.Employee;
+
+public interface EmployeeRepository {
+
+    ArrayList<Employee> getAllEmployees();
+
+    Employee getEmployeeById(int employeeId);
+
+    Employee addEmployee(Employee employee);
+
+    Employee updateEmployee(int employeeId, Employee employee);
+
+    void deleteEmployee(int employeeId);
+}
